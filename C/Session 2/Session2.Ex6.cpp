1edf0a09d41441747639846f04c28e2b9a40dcb2
hoanghocit <vduchoang2006@gmail.com>
2024-11-19 16:54:51 +0700

    Add files via upload

--- C/Session 2/Session2.Ex6.cpp
+++ C/Session 2/Session2.Ex6.cpp
@@ -3,8 +3,8 @@ int main(){
 float pi = 3.14;
 int bankinh = 5;
 printf("Ban Kinh Hinh Tron: %d \n", bankinh);
-int chuvi = 2 * pi * bankinh;
-printf("Chu Vi Hinh Tron: 2 x %f x %d = %d \n", pi, bankinh, chuvi);
+float chuvi = 2 * pi * bankinh;
+printf("Chu Vi Hinh Tron: 2 x %f x %d = %f \n", pi, bankinh, chuvi);
 float dientich = pi * bankinh * bankinh;
 printf("Dien Tich Hinh Tron: %f x %d^2 = %f \n", pi, bankinh, dientich);
 return 0;
