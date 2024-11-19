978f65913821d99a39c43c2f941f53f4fbb110cd
hoanghocit <vduchoang2006@gmail.com>
2024-11-19 16:39:49 +0700

    Add files via upload

---
+++ Session 2/Session2.Ex2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int number_1 = 100;
+	int number_2 = 500;
+	int tong = number_1 + number_2;
+	printf("Tong Cua 2 So La %d \n", tong);
+	int hieu = number_2 - number_1;
+	printf("Hieu Cua 2 So La %d \n", hieu);
+	int tich = number_1 * number_2;
+	printf("Tich Cua 2 So La %d \n", tich);
+	int thuong = number_2 / number_1;
+	printf("Thuong Cua 2 So La %d \n", thuong);
+return 0;
+}
---
+++ Session 2/Session2.Ex4.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int canh = 5;
+printf("Chieu Canh Hinh Vuong: %d \n", canh);
+int chuvi = 4 * canh;
+printf("Chu Vi Hinh Vuong: 4 x %d = %d \n", canh, chuvi);
+int dientich = canh * canh;
+printf("Dien Tich Hinh Vuong: %d x %d = %d \n", canh, canh, dientich);
+return 0;
+}
