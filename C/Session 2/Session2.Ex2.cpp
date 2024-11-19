5913a37d6010bba8a7a0bfd004697723c857da41
hoanghocit <vduchoang2006@gmail.com>
2024-11-19 17:11:31 +0700

    Add files via upload

--- C/Session 2/Session2.Ex2.cpp
+++ C/Session 2/Session2.Ex2.cpp
@@ -4,11 +4,5 @@ int main(){
 	int number_2 = 500;
 	int tong = number_1 + number_2;
 	printf("Tong Cua 2 So La %d \n", tong);
-	int hieu = number_2 - number_1;
-	printf("Hieu Cua 2 So La %d \n", hieu);
-	int tich = number_1 * number_2;
-	printf("Tich Cua 2 So La %d \n", tich);
-	int thuong = number_2 / number_1;
-	printf("Thuong Cua 2 So La %d \n", thuong);
 return 0;
 }
