31d0e74a9f7d82adc32625b40fea8137c5e22269
hoanghocit <vduchoang2006@gmail.com>
2024-11-21 18:51:56 +0700

    Delete C/Session3.Ex1.cpp

--- C/Session3.Ex1.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-fflush(stdin);
-char fullName[50];
-printf("Ban Hay Nhap Ten Cua Ban: ", fullName);
-fgets(fullName, sizeof(fullName), stdin);
-printf("Xin Chào %s \n", fullName);
-return 0;
-}
