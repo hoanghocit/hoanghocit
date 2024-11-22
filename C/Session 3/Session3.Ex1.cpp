ff5ce61570f91787bbb8746a09626c69fa58791b
hoanghocit <vduchoang2006@gmail.com>
2024-11-22 07:52:58 +0700

    Update Session3.Ex1.cpp

--- C/Session 3/Session3.Ex1.cpp
+++ C/Session 3/Session3.Ex1.cpp
@@ -3,7 +3,7 @@ int main(){
 fflush(stdin);
 char fullName[50];
 printf("Ban Hay Nhap Ten Cua Ban: ", fullName);
-fgets(fullName, sizeof(fullName), stdin);
-printf("Xin Ch‡o %s \n", fullName);
+scanf("%s",&fullName);
+printf("Xin Ch√†o %s \n", fullName);
 return 0;
 }
